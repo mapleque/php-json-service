6b609998d45fb07252d767aec59315e99290466c
mapleque <mapleque@163.com>
2015-08-26 12:21:23 +0800

    new

---
+++ README.md
@@ -0,0 +1,2 @@
+#这是一个php web service 框架
+
---
+++ config/important.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+class Important
+{
+	const DOMAIN = '';
+	const DB_ADDR = '';
+	const DB_PORT= '';
+	const DB_USER= '';
+	const DB_PASS= '';
+	const DB_NAME= '';
+}
